fb0c0c03828ac44ccf12e93220369f9a78604e73
DylanCahuMDS <dylan.cahu@my-digital-shcool.org>
2023-12-02 09:43:12 +0100

    recursivity

--- index.php
+++ index.php
@@ -1,27 +1,34 @@
 <?php
+use function usleep;
 
-$helloWorld = str_split("hello world");
-$alphabet = str_split("abcdefghijklmnopqrstuvwxyz");
-
-$display = array_fill(0, 11, '');
-
-for ($i = 0; $i < 11; $i++) {
-    if ($i == 5) {
-        $display[$i] = ' '; // whitespace
-    } else {
-        $letter = $alphabet[array_rand($alphabet)];
-        while ($helloWorld[$i] != $letter) {
-            $letter = $alphabet[array_rand($alphabet)];
-            $display[$i] = $letter;
-            echo implode($display) . "\n";
-            usleep(15000); // sleep for 15 milliseconds
-        }
-        if ($i == 0 || $i == 6) {
-            $display[$i] = strtoupper($letter); // first letter of each word in uppercase
+function helloWorld() {
+   
+    $helloWorld = str_split("hello world");
+    $alphabet = str_split("abcdefghijklmnopqrstuvwxyz");
+    
+    $display = array_fill(0, 11, '');
+    
+    for ($i = 0; $i < 11; $i++) {
+        if ($i == 5) {
+            $display[$i] = ' '; // whitespace
         } else {
-            $display[$i] = $letter;
+            $letter = $alphabet[array_rand($alphabet)];
+            while ($helloWorld[$i] != $letter) {
+                $letter = $alphabet[array_rand($alphabet)];
+                $display[$i] = $letter;
+                echo implode($display) . "\n";
+                usleep(15000); // sleep for 15 milliseconds
+            }
+            if ($i == 0 || $i == 6) {
+                $display[$i] = strtoupper($letter); // first letter of each word in uppercase
+            } else {
+                $display[$i] = $letter;
+            }
         }
     }
+    sleep(2);
+    helloWorld();    
 }
+
 
 ?>
